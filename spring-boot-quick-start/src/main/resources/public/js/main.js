b2c8f9ce0499ee45a6b33727837d3e6ffb5c4c8c
Evgeniy Shvetsov <venedey.e@gmail.com>
2018-04-19 18:19:27 +0300

    spring-boot-quick-start: mvc pattern added

--- spring-boot-quick-start/src/main/resources/public/js/main.js
+++ spring-boot-quick-start/src/main/resources/public/js/main.js
@@ -1,11 +1,8 @@
 var app = angular.module('springDemo', []);
 
-app.controller('AppCtrl', function($scope){
-    $scope.websites = [{
-        iconImageUrl: '',
-        id: 'StackoverFlow',
-        website: 'stackoverflow.com',
-        title: 'StackOverflow website',
-        description: 'StackOverflow description'
-    }];
+app.controller('AppCtrl', function($scope, $http){
+    $scope.websites = [];
+    $http.get('http://localhost:8090/api/stackoverflow').success(function(data){
+        $scope.websites = data;
+    });
 });
